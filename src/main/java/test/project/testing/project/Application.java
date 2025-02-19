16aa93ca5feb9268601367c237475eeca07974b1
FDRMJavaStudent <email@example.com>
2025-02-19 18:17:31 +0900

    初回

---
+++ src/main/java/test/project/testing/project/Application.java
@@ -0,0 +1,20 @@
+package test.project.testing.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/imsleepy")
+  public String hello() {
+    return "Wake up";
+  }
+}
