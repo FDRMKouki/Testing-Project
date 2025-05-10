d82a026d9a61fd176e89fdc7c8a09eab681f9b88
FDRMJavaStudent <email@example.com>
2025-05-10 11:39:42 +0900

    Rest化する前の差分です

--- src/main/java/lesson/project/studentsmanagement/project/controller/StudentController.java
+++ src/main/java/lesson/project/studentsmanagement/project/controller/StudentController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
+  
 
   private StudentService service;
   private StudentConverter converter;
