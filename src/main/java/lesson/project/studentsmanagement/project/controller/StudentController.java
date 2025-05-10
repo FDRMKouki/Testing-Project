d744025dcfac5e015b11b42f680a9da0c79574f3
FDRMJavaStudent <email@example.com>
2025-05-10 11:43:12 +0900

    Rest化最中のをマージ

--- src/main/java/lesson/project/studentsmanagement/project/controller/StudentController.java
+++ src/main/java/lesson/project/studentsmanagement/project/controller/StudentController.java
@@ -115,7 +115,7 @@ public class StudentController {
     return ResponseEntity.ok("こうしんしょりせいこおお");
   }
 
-  //----生徒論理削除----
+  //----生徒論理削除-----
   @PostMapping("/deleteStudent")
   public String deleteStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
     System.out.println("削除された生徒Id:" + studentDetail.getStudent().getId());
