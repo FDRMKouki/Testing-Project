2da056710fa2d273d3afaff17b73ff546d0f5de2
FDRMJavaStudent <email@example.com>
2025-07-01 12:09:19 +0900

    リポジトリテスと中

---
+++ src/test/java/lesson/project/studentsmanagement/project/repository/StudentRepositoryTest.java
@@ -0,0 +1,14 @@
+package lesson.project.studentsmanagement.project.repository;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+
+@MybatisTest
+class StudentRepositoryTest {
+
+  @Test
+  void test() {
+
+  }
+
+}
